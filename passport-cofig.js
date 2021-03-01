67fb8f1e10d2ba9f7225a6d798eca1273254bf61
selirose <seli.rosriana@gmail.com>
2021-03-01 15:17:59 +0700

    using passport for login and register

--- passport-cofig.js
+++ passport-cofig.js
@@ -1,27 +1,29 @@
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
 
-function initialize() {
-    const authenticateUser = (email, password, done) => {
+function initialize(passport, getUserByEmail, getUserById) {
+    const authenticateUser = async (email, password, done) => {
         const user = getUserByEmail(email)
-        if (user ==null){
-            return done(null, false, {message: 'NO user with that email'})
+        if (user == null){
+            return done(null, false, {messages: 'NO user with that email'})
         }
 
         try{
-            if(await bcrypt.compare(password, user.password)) {
+            if (await bcrypt.compare(password, user.password)) {
                 return done(null, user)
             } else {
-                return done(null, false, { message: 'Password Incorrect'})
+                return done(null, false, { messages: 'Password Incorrect'})
             }
         } catch (e) {
             return done(e)
         }
     }
 
-    passport.use(new LocalStrategy({ usernameField: 'email'}), authenticateUser)
-    passport.serializeuser((user, done) => { })
-    passport.deserializeUser((id, done) => { })
+    passport.use(new LocalStrategy({ usernameField: 'email'}, authenticateUser))
+    passport.serializeUser((user, done) => done(null, user.id)) 
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id))
+    })
 }
 
 module.exports = initialize
